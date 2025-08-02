c6229c2ed23b50ea70ea0fbe9b4d16178d5a7b1c
habin <habinrahman@example.com>
2025-08-02 18:13:01 +0530

    Add send_emails.py

---
+++ send_emails.py
@@ -0,0 +1,3 @@
+import datetime
+with open("C:\\Users\\Hp\\PycharmProjects\\pythonProject3\\task_scheduler_test_output.txt", "a") as f:
+    f.write(f"Test run successful at {datetime.datetime.now()}\n")
